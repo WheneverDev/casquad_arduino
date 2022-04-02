5149f8bea48a20a637d413a0a0fba7d823ad12ae
WheneverDev <whenever.fansub@gmail.com>
2022-04-02 19:12:45 +0200

    Rename main file and rearrange code

--- CasquAD BLE/CasquAD_BLE.ino/CasquAD_BLE.ino
+++ CasquAD BLE/CasquAD_BLE.ino/CasquAD_BLE.ino
@@ -1,11 +1,7 @@
 #include <ArduinoBLE.h> 
+#include "config.h"
 
-#define PIN_LED 2
-#define PIN_BUTTON 4
-
-#define SERVICE_UUID "ee04e830-6709-4f42-af61-3e66c48918b3"
-#define ANALOG_CHARACT_UUID "ee04e831-6709-4f42-af61-3e66c48918b3"
-#define CHANNEL_CHARACT_UUID "ee04e832-6709-4f42-af61-3e66c48918b3"
+int lastButtonStatus;
 
 BLEService AnalogService(SERVICE_UUID);
 BLECharacteristic AnalogChar(ANALOG_CHARACT_UUID, BLERead | BLENotify, 2);
@@ -14,12 +10,10 @@ BLEByteCharacteristic ChannelChar(CHANNEL_CHARACT_UUID, BLEWrite | BLEWrite);
 BLEDescriptor AnalogDescriptor("2901","analog");
 BLEDescriptor ChannelDescriptor("2901","channel");
 
-boolean write_adr;
-int lastButtonStatus;
-
 void setup() {
 	Serial.begin(115200);
 	analogReadResolution(12);
+
 	pinMode(PIN_LED, OUTPUT);
 	pinMode(PIN_BUTTON, INPUT_PULLUP);
 
@@ -45,17 +39,12 @@ void setup() {
 
 void loop() {
 
-	if (write_adr) {
-		Serial.print("Adresse : ");
-		Serial.println(BLE.address());
-	}
 	BLEDevice central = BLE.central();
 	if (central) {
 		Serial.print("Connected to central : ");
 		Serial.println(central.address());
 
 		while (central.connected()) {
-
         uint16_t button = digitalRead(PIN_BUTTON);
         if (lastButtonStatus == 0 && button == 1) {
           Serial.println("Button push!");
@@ -63,22 +52,17 @@ void loop() {
         lastButtonStatus = button;
 
 				if (ChannelChar.written()) {
-						Serial.println("Channel char written");
-						int c = ChannelChar.value();
-
-						if ((c>=0)&&(c<=8)) {
-							Serial.println(c);
-
-							if(c == 4){
-								digitalWrite(PIN_LED, HIGH);
-								Serial.println("LED");
-							}
-						}
+					Serial.println("Channel char written");
+					int c = ChannelChar.value();
+					if(c == 4){
+						digitalWrite(PIN_LED, !digitalRead(PIN_LED));
+						Serial.println("LED");
+					}
 				}
 				
 				AnalogChar.writeValue(button);
 
-				delay(1000);
+				delay(250);
 		}
 
 		Serial.print("Disconnected from central: ");
